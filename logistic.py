daceaab79b02e6127bc102fcde10f43aa1958328
Jonathan Chia <jonathan.chia@u.yale-nus.edu.sg>
2023-02-14 20:37:42 +0800

    commit

--- logistic.py
+++ logistic.py
@@ -22,19 +22,21 @@ from pylab import scatter, show, legend, xlabel, ylabel
 # scale larger positive and values to between -1,1 depending on the largest
 # value in the data
 min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
-df = pd.read_csv("data.csv", header=0)
+df = pd.read_excel("telco_customer_churn.xlsx",sheet_name = "telco_customer_churn", header=0)
 
 # clean up data
-df.columns = ["grade1","grade2","label"]
-
-x = df["label"].map(lambda x: float(x.rstrip(';')))
+df["gender"] = pd.get_dummies(df["gender"])
+df["partner"] = pd.get_dummies(df["partner"])
+df["payment_mode"] = pd.get_dummies(df["payment_mode"])
+df["contract_type"] = pd.get_dummies(df["contract_type"])
+df["churn"] = pd.get_dummies(df["churn"])
 
 # formats the input data into two arrays, one of independant variables
 # and one of the dependant variable
-X = df[["grade1","grade2"]]
+X = df[["gender","partner","payment_mode","contract_type","tenure","charges_per_month"]]
 X = np.array(X)
 X = min_max_scaler.fit_transform(X)
-Y = df["label"].map(lambda x: float(x.rstrip(';')))
+Y = df["churn"]
 Y = np.array(Y)
 
 
@@ -54,12 +56,12 @@ print ('score Scikit learn: ', clf.score(X_test,Y_test))
 # visualize data, uncomment "show()" to run it
 pos = where(Y == 1)
 neg = where(Y == 0)
-scatter(X[pos, 0], X[pos, 1], marker='o', c='b')
-scatter(X[neg, 0], X[neg, 1], marker='x', c='r')
-xlabel('Exam 1 score')
-ylabel('Exam 2 score')
-legend(['Not Admitted', 'Admitted'])
-show()
+#scatter(X[pos, 0], X[pos, 1], marker='o', c='b')
+#scatter(X[neg, 0], X[neg, 1], marker='x', c='r')
+#xlabel('Exam 1 score')
+#ylabel('Exam 2 score')
+#legend(['Not Admitted', 'Admitted'])
+#show()
 
 ##The sigmoid function adjusts the cost function hypotheses to adjust the algorithm proportionally for worse estimations
 def Sigmoid(z):
@@ -70,7 +72,7 @@ def Sigmoid(z):
 ##This hypothesis will be used to calculate each instance of the Cost Function
 def Hypothesis(theta, x):
 	z = 0
-	for i in xrange(len(theta)):
+	for i in range(len(theta)):
 		z += x[i]*theta[i]
 	return Sigmoid(z)
 
@@ -79,7 +81,7 @@ def Hypothesis(theta, x):
 ##The "punishment" makes the change in the gradient of ThetaCurrent - Average(CostFunction(Dataset)) greater
 def Cost_Function(X,Y,theta,m):
 	sumOfErrors = 0
-	for i in xrange(m):
+	for i in range(m):
 		xi = X[i]
 		hi = Hypothesis(theta,xi)
 		if Y[i] == 1:
@@ -98,7 +100,7 @@ def Cost_Function(X,Y,theta,m):
 ##For each Theta there is a cost function calculated for each member of the dataset
 def Cost_Function_Derivative(X,Y,theta,j,m,alpha):
 	sumErrors = 0
-	for i in xrange(m):
+	for i in range(m):
 		xi = X[i]
 		xij = xi[j]
 		hi = Hypothesis(theta,X[i])
@@ -115,7 +117,7 @@ def Cost_Function_Derivative(X,Y,theta,j,m,alpha):
 def Gradient_Descent(X,Y,theta,m,alpha):
 	new_theta = []
 	constant = alpha/m
-	for j in xrange(len(theta)):
+	for j in range(len(theta)):
 		CFDerivative = Cost_Function_Derivative(X,Y,theta,j,m,alpha)
 		new_theta_value = theta[j] - CFDerivative
 		new_theta.append(new_theta_value)
@@ -126,7 +128,7 @@ def Gradient_Descent(X,Y,theta,m,alpha):
 ##set of values best representing the system in a linear combination model
 def Logistic_Regression(X,Y,alpha,theta,num_iters):
 	m = len(Y)
-	for x in xrange(num_iters):
+	for x in range(num_iters):
 		new_theta = Gradient_Descent(X,Y,theta,m,alpha)
 		theta = new_theta
 		if x % 100 == 0:
@@ -144,7 +146,7 @@ def Declare_Winner(theta):
     #if the prediction is the same as the dataset measured value it counts as a point for thie scikit version of LR
     scikit_score = clf.score(X_test,Y_test)
     length = len(X_test)
-    for i in xrange(length):
+    for i in range(length):
         prediction = round(Hypothesis(X_test[i],theta))
         answer = Y_test[i]
         if prediction == answer:
